a8b1c11759c7efa5d06676b6121968a09d6566c5
Andrei-David Nan <nandavid190@gmail.com>
2023-12-02 19:54:48 +0200

    v0.0.1 - initial commit;

---
+++ src/main/java/io/rewardsapp/MsgRecyclingRewardsBackendApplication.java
@@ -0,0 +1,13 @@
+package io.rewardsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsgRecyclingRewardsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsgRecyclingRewardsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/rewardsapp/MsgRecyclingRewardsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.rewardsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsgRecyclingRewardsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
