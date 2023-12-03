4da2b615c9588eea4b71935bddb490b14cf3f75f
Andrei-David Nan <nandavid190@gmail.com>
2023-12-03 15:36:25 +0200

    v0.0.9 - model domain.HttpResponse entity;

---
+++ src/main/java/io/rewardsapp/domain/HttpResponse.java
@@ -0,0 +1,23 @@
+package io.rewardsapp.domain;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
+import org.springframework.http.HttpStatus;
+
+import java.util.Map;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_DEFAULT;
+
+@Data
+@SuperBuilder
+@JsonInclude(NON_DEFAULT)
+public class HttpResponse {
+    protected String timeStamp;
+    protected int statusCode;
+    protected HttpStatus status;
+    protected String reason;
+    protected String message;
+    protected String _devMessage;
+    protected Map<?, ?> data;
+}
