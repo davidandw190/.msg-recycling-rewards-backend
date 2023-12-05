555aa8cd1937212f1dc34558af67290d72228038
Andrei-David Nan <nandavid190@gmail.com>
2023-12-05 11:44:10 +0200

    v0.1.8 - add createRefreshToken method in TokenProvider;

--- src/main/java/io/rewardsapp/provider/TokenProvider.java
+++ src/main/java/io/rewardsapp/provider/TokenProvider.java
@@ -5,15 +5,18 @@ import com.auth0.jwt.algorithms.Algorithm;
 import io.rewardsapp.domain.UserPrincipal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
 import java.util.Date;
 
+import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
+
 @Component
 @RequiredArgsConstructor
 public class TokenProvider {
-    private static final String CUSTOMER_MANAGEMENT_SERVICE = ".msg Systems Customer Service";
+    private static final String MSG_CUSTOMER_SERVICE = ".msg Systems Customer Service";
     private static final String MSG_SYSTEMS_ROMANIA = ".msg Systems Romania";
     private static final String AUTHORITIES = "authorities";
 
@@ -32,12 +35,33 @@ public class TokenProvider {
     public String createAccessToken(UserPrincipal userPrincipal) {
         return JWT.create()
                 .withIssuer(MSG_SYSTEMS_ROMANIA)
-                .withAudience(CUSTOMER_MANAGEMENT_SERVICE)
+                .withAudience(MSG_CUSTOMER_SERVICE)
                 .withIssuedAt(Date.from(Instant.now()))
                 .withSubject(String.valueOf(userPrincipal.user().getId()))
                 .withArrayClaim(AUTHORITIES, getClaimsFromUser(userPrincipal))
                 .withExpiresAt(Date.from(Instant.now().plusMillis(ACCESS_TOKEN_EXPIRATION_TIME)))
                 .sign(Algorithm.HMAC512(SECRET.getBytes()));
+    }
+
+    /**
+     * Creates a refresh token for the given user principal.
+     *
+     * @param userPrincipal The user principal for whom the refresh token is created.
+     * @return The generated refresh token.
+     */
+    public String createRefreshToken(UserPrincipal userPrincipal) {
+        return JWT.create()
+                .withIssuer(MSG_SYSTEMS_ROMANIA)
+                .withAudience(MSG_CUSTOMER_SERVICE)
+                .withIssuedAt(Date.from(Instant.now()))
+                .withSubject(String.valueOf(userPrincipal.user().getId()))
+                .withExpiresAt(Date.from(Instant.now().plusMillis(REFRESH_TOKEN_EXPIRATION_TIME)))
+                .sign(HMAC512(SECRET.getBytes()));
+    }
+
+    /* Retrieves the claims associated with a user principal. */
+    private String[] getClaimsFromUser(UserPrincipal userPrincipal) {
+        return userPrincipal.getAuthorities().stream().map(GrantedAuthority::getAuthority).toArray(String[]::new);
     }
 
 }
