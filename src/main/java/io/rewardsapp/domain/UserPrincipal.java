28202335ab5228a79b403c74a67e8dd0224c4188
Andrei-David Nan <nandavid190@gmail.com>
2023-12-05 11:28:02 +0200

    v0.1.5 - set up domain.UserPrincipal;

---
+++ src/main/java/io/rewardsapp/domain/UserPrincipal.java
@@ -0,0 +1,48 @@
+package io.rewardsapp.domain;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+/**
+ * The UserPrincipal class represents the authenticated principal (user) in the security context.
+ */
+public record UserPrincipal(User user, Role role) implements UserDetails {
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return AuthorityUtils.commaSeparatedStringToAuthorityList(role.getPermission());
+    }
+
+    @Override
+    public String getPassword() {
+        return this.user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return this.user.isNotLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.user.isEnabled();
+    }
+}
