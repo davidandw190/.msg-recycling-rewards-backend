a70423ba096e732f0df5fd2147c0b790ca3c12c2
Andrei-David Nan <nandavid190@gmail.com>
2023-12-07 19:59:41 +0200

    v0.2.8 - configure getRowByUserId flow in RoleServiceImpl and RoleRepositoryImpl;

--- src/main/java/io/rewardsapp/repository/implementation/RoleRepositoryImpl.java
+++ src/main/java/io/rewardsapp/repository/implementation/RoleRepositoryImpl.java
@@ -1,17 +1,27 @@
 package io.rewardsapp.repository.implementation;
 
 import io.rewardsapp.domain.Role;
+import io.rewardsapp.exception.ApiException;
 import io.rewardsapp.repository.RoleRepository;
+import io.rewardsapp.rowmapper.RoleRowMapper;
 import lombok.extern.slf4j.Slf4j;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
+
+import static io.rewardsapp.enums.RoleType.ROLE_USER;
+import static io.rewardsapp.query.RoleQuery.SELECT_ROLE_BY_NAME_QUERY;
+import static java.util.Map.of;
 
 @Slf4j
 @Repository
 @RequiredArgsConstructor
 public class RoleRepositoryImpl implements RoleRepository<Role> {
+    private final NamedParameterJdbcTemplate jdbc;
+
     @Override
     public Role create(Role data) {
         return null;
@@ -44,7 +54,17 @@ public class RoleRepositoryImpl implements RoleRepository<Role> {
 
     @Override
     public Role getRoleByUserId(Long userId) {
-        return null;
+        log.info("Fetching role for user id: {}", userId);
+        try {
+            return jdbc.queryForObject(SELECT_ROLE_BY_NAME_QUERY, of("userId", userId), new RoleRowMapper());
+
+        } catch (EmptyResultDataAccessException exception) {
+            throw new ApiException("No role found by name: " + ROLE_USER.name());
+
+        } catch (Exception exception) {
+            log.error(exception.getMessage());
+            throw new ApiException("An error occurred. Please try again.");
+        }
     }
 
     @Override
--- src/main/java/io/rewardsapp/service/implementation/RoleServiceImpl.java
+++ src/main/java/io/rewardsapp/service/implementation/RoleServiceImpl.java
@@ -1,6 +1,7 @@
 package io.rewardsapp.service.implementation;
 
 import io.rewardsapp.domain.Role;
+import io.rewardsapp.repository.RoleRepository;
 import io.rewardsapp.service.RoleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,8 +9,10 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class RoleServiceImpl implements RoleService {
+    private final RoleRepository<Role> roleRoleRepository;
+
     @Override
     public Role getRoleByUserId(Long id) {
-        return null;
+        return roleRoleRepository.getRoleByUserId(id);
     }
 }
