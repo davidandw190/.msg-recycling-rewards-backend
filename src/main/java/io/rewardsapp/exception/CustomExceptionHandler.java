e5f0923dbfd1d021189e99e9788c953fd133c623
Andrei-David Nan <nandavid190@gmail.com>
2023-12-09 23:17:46 +0200

    v0.3.0 - add CustomExceptionHandler for Global Exception Handling;

---
+++ src/main/java/io/rewardsapp/exception/CustomExceptionHandler.java
@@ -0,0 +1,51 @@
+package io.rewardsapp.exception;
+
+import io.rewardsapp.domain.HttpResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.http.HttpStatus.resolve;
+
+@Slf4j
+@RestControllerAdvice
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler implements ErrorController {
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception exception, Object body, HttpHeaders headers, HttpStatusCode statusCode, WebRequest request) {
+        log.error(exception.getMessage());
+        return new ResponseEntity<>(
+                HttpResponse.builder()
+                        .timeStamp(LocalDateTime.now().toString())
+                        .reason(exception.getMessage())
+                        ._devMessage(exception.getMessage())
+                        .status(resolve(statusCode.value()))
+                        .statusCode(statusCode.value())
+                        .build(), statusCode);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception, HttpHeaders headers, HttpStatusCode statusCode, WebRequest request) {
+        log.error(exception.getMessage());
+        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+        String fieldMessage = fieldErrors.stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(", "));
+        return new ResponseEntity<>(
+                HttpResponse.builder()
+                        .timeStamp(LocalDateTime.now().toString())
+                        .reason(fieldMessage)
+                        ._devMessage(exception.getMessage())
+                        .status(resolve(statusCode.value()))
+                        .statusCode(statusCode.value())
+                        .build(), statusCode);
+    }
+}
