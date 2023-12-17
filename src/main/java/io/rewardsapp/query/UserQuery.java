cfb953ef7c3264ddf10929e970f17f4c802a8307
Andrei-David Nan <nandavid190@gmail.com>
2023-12-17 13:48:34 +0200

    v0.3.18 - fix user details update query;

--- src/main/java/io/rewardsapp/query/UserQuery.java
+++ src/main/java/io/rewardsapp/query/UserQuery.java
@@ -13,7 +13,7 @@ public class UserQuery {
     public static final String COUNT_USER_EMAIL_QUERY = "SELECT COUNT(*) FROM " + DB_NAME + ".users WHERE email = :email";
     public static final String DELETE_VERIFICATION_CODE_BY_USER_ID = "DELETE FROM " + DB_NAME + ".tfa_verifications WHERE user_id = :userId";
     public static final String INSERT_VERIFICATION_CODE_QUERY = "INSERT INTO " + DB_NAME + ".tfa_verifications (user_id, code, expiration_date) VALUES (:userId, :code, :expirationDate)";
-    public static final String UPDATE_USER_DETAILS_QUERY = "UPDATE " + DB_NAME + ".users SET first_name = :firstName, last_name = :lastName, email = :email, city = :city, phone = :phone, address = :address, title = :title, bio = :bio WHERE user_id = :user_id";
+    public static final String UPDATE_USER_DETAILS_QUERY = "UPDATE " + DB_NAME + ".users SET first_name = :firstName, last_name = :lastName, email = :email, city = :city, phone = :phone, address = :address, bio = :bio WHERE user_id = :user_id";
     public static final String SELECT_USER_BY_USER_CODE_QUERY = "SELECT * FROM " + DB_NAME + ".users WHERE user_id = (SELECT user_id FROM " + DB_NAME + ".tfa_verifications WHERE code = :code)";
     public static final String DELETE_CODE = "DELETE FROM " + DB_NAME + ".tfa_verifications WHERE code = :code";
     public static final String SELECT_CODE_EXPIRATION_QUERY = "SELECT expiration_date < NOW() AS is_expired FROM " + DB_NAME + ".tfa_verifications WHERE code = :code";
