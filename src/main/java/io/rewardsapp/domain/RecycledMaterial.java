149cca60baa34e523c1f26bd27ccd9cb230e471c
Andrei-David Nan <nandavid190@gmail.com>
2023-12-19 12:17:29 +0200

    v0.4.2 - configure domain.RecycledMaterial entity;

---
+++ src/main/java/io/rewardsapp/domain/RecycledMaterial.java
@@ -0,0 +1,29 @@
+package io.rewardsapp.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.util.Set;
+
+@Entity
+@SuperBuilder
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "recycled_materials")
+public class RecycledMaterial {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "material_id")
+    private Long materialId;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @OneToMany(mappedBy = "recycledMaterial", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<UserRecyclingActivity> recyclingActivities;
+}
