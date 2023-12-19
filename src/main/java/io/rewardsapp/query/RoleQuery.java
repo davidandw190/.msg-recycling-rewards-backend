9172597b2176fb75f8f51773553e87abb14cfa4f
Andrei-David Nan <nandavid190@gmail.com>
2023-12-19 12:14:27 +0200

    v0.4.0 - set up schema configuration in config file;

--- src/main/java/io/rewardsapp/query/RoleQuery.java
+++ src/main/java/io/rewardsapp/query/RoleQuery.java
@@ -1,17 +1,13 @@
 package io.rewardsapp.query;
 
-import org.springframework.beans.factory.annotation.Value;
-
 /**
  * Contains SQL queries related to roles in the SnapInvoice platform.
  */
 public class RoleQuery {
-    @Value("${spring.datasource.name}")
-    private static String DB_NAME;
 
-    public static final String SELECT_ROLES_QUERY = "SELECT * FROM " + DB_NAME + ".roles ORDER BY role_id";
-    public static final String INSERT_ROLE_TO_USER_QUERY = "INSERT INTO " + DB_NAME + ".user_roles (user_id, role_id) VALUES (:userId, :roleId)";
-    public static final String SELECT_ROLE_BY_NAME_QUERY = "SELECT * FROM " + DB_NAME + ".roles WHERE name = :name";
-    public static final String SELECT_ROLE_BY_ID_QUERY = "SELECT r.role_id, r.name, r.permission FROM " + DB_NAME + ".roles r JOIN " + DB_NAME + ".user_roles ur ON ur.role_id = r.role_id JOIN " + DB_NAME + ".users u ON u.user_id = ur.user_id WHERE u.user_id = :userId";
-    public static final String UPDATE_USER_ROLE_QUERY = "UPDATE " + DB_NAME + ".user_roles SET role_id = :roleId WHERE user_id = :userId";
+    public static final String SELECT_ROLES_QUERY = "SELECT * FROM roles ORDER BY role_id";
+    public static final String INSERT_ROLE_TO_USER_QUERY = "INSERT INTO user_roles (user_id, role_id) VALUES (:userId, :roleId)";
+    public static final String SELECT_ROLE_BY_NAME_QUERY = "SELECT * FROM roles WHERE name = :name";
+    public static final String SELECT_ROLE_BY_ID_QUERY = "SELECT r.role_id, r.name, r.permission FROM roles r JOIN user_roles ur ON ur.role_id = r.role_id JOIN users u ON u.user_id = ur.user_id WHERE u.user_id = :userId";
+    public static final String UPDATE_USER_ROLE_QUERY = "UPDATE user_roles SET role_id = :roleId WHERE user_id = :userId";
 }
--- src/main/java/io/rewardsapp/query/UserQuery.java
+++ src/main/java/io/rewardsapp/query/UserQuery.java
@@ -1,35 +1,31 @@
 package io.rewardsapp.query;
 
-import org.springframework.beans.factory.annotation.Value;
-
 /**
  * Contains queries related to User entities.
  */
 public class UserQuery {
-    @Value("${spring.datasource.name}")
-    private static String DB_NAME;
 
-    public static final String INSERT_USER_QUERY = "INSERT INTO " + DB_NAME + ".users (first_name, last_name, email, password, city) VALUES (:firstName, :lastName, :email, :password, :city)";
-    public static final String SELECT_USER_BY_EMAIL_QUERY = "SELECT * FROM " + DB_NAME + ".users WHERE email = :email";
-    public static final String INSERT_VERIFICATION_QUERY = "INSERT INTO " + DB_NAME + ".account_verifications (user_id, url) VALUES (:userId, :url)";
-    public static final String SELECT_USER_BY_ID_QUERY = "SELECT * FROM " + DB_NAME + ".users WHERE user_id = :userId";
-    public static final String COUNT_USER_EMAIL_QUERY = "SELECT COUNT(*) FROM " + DB_NAME + ".users WHERE email = :email";
-    public static final String DELETE_VERIFICATION_CODE_BY_USER_ID = "DELETE FROM " + DB_NAME + ".tfa_verifications WHERE user_id = :userId";
-    public static final String INSERT_VERIFICATION_CODE_QUERY = "INSERT INTO " + DB_NAME + ".tfa_verifications (user_id, code, expiration_date) VALUES (:userId, :code, :expirationDate)";
-    public static final String UPDATE_USER_DETAILS_QUERY = "UPDATE " + DB_NAME + ".users SET first_name = :firstName, last_name = :lastName, email = :email, city = :city, phone = :phone, address = :address, bio = :bio WHERE user_id = :user_id";
-    public static final String SELECT_USER_BY_USER_CODE_QUERY = "SELECT * FROM " + DB_NAME + ".users WHERE user_id = (SELECT user_id FROM " + DB_NAME + ".tfa_verifications WHERE code = :code)";
-    public static final String DELETE_CODE = "DELETE FROM " + DB_NAME + ".tfa_verifications WHERE code = :code";
-    public static final String SELECT_CODE_EXPIRATION_QUERY = "SELECT expiration_date < NOW() AS is_expired FROM " + DB_NAME + ".tfa_verifications WHERE code = :code";
-    public static final String DELETE_PASSWORD_VERIFICATION_BY_USER_ID_QUERY = "DELETE FROM " + DB_NAME + ".reset_pass_verifications WHERE user_id = :userId";
-    public static final String INSERT_PASSWORD_VERIFICATION_QUERY = "INSERT INTO " + DB_NAME + ".reset_pass_verifications (user_id, url, expiration_date) VALUES (:userId, :url, :expirationDate)";
-    public static final String SELECT_USER_BY_PASSWORD_URL_QUERY = "SELECT * FROM " + DB_NAME + ".users WHERE user_id = (SELECT user_id FROM " + DB_NAME + ".reset_pass_verifications WHERE url = :url)";
-    public static final String SELECT_EXPIRATION_BY_URL = "SELECT expiration_date < NOW() AS is_expired FROM " + DB_NAME + ".reset_pass_verifications WHERE url = :url";
-    public static final String UPDATE_USER_PASSWORD_BY_USER_ID_QUERY = "UPDATE " + DB_NAME + ".users SET password = :password WHERE user_id = :userId";
-    public static final String UPDATE_USER_PASSWORD_BY_ID_QUERY = "UPDATE " + DB_NAME + ".users SET password = :password WHERE user_id = :userId";
-    public static final String SELECT_USER_BY_ACCOUNT_URL_QUERY = "SELECT * FROM " + DB_NAME + ".users WHERE user_id = (SELECT user_id FROM " + DB_NAME + ".account_verifications WHERE url = :url)";
-    public static final String UPDATE_USER_ENABLED_QUERY = "UPDATE " + DB_NAME + ".users SET enabled = :enabled WHERE userId = :userId";
-    public static final String TOGGLE_USER_MFA_QUERY = "UPDATE " + DB_NAME + ".users SET using_mfa = :isUsingMfa WHERE email = :email";
-    public static final String UPDATE_USER_SETTINGS_QUERY = "UPDATE " + DB_NAME + ".users SET enabled = :enabled, non_locked = :notLocked WHERE user_id = :userId";
-    public static final String TOGGLE_USER_NOTIFICATIONS_QUERY = "UPDATE " + DB_NAME + ".users SET notif_enabled = :notifEnabled WHERE email = :email";
-    public static final String UPDATE_USER_PROFILE_IMAGE_QUERY = "UPDATE " + DB_NAME + ".users SET image_url = :imageUrl WHERE user_id = :userId";
+    public static final String INSERT_USER_QUERY = "INSERT INTO users (first_name, last_name, email, password, city) VALUES (:firstName, :lastName, :email, :password, :city)";
+    public static final String SELECT_USER_BY_EMAIL_QUERY = "SELECT * FROM users WHERE email = :email";
+    public static final String INSERT_VERIFICATION_QUERY = "INSERT INTO account_verifications (user_id, url) VALUES (:userId, :url)";
+    public static final String SELECT_USER_BY_ID_QUERY = "SELECT * FROM users WHERE user_id = :userId";
+    public static final String COUNT_USER_EMAIL_QUERY = "SELECT COUNT(*) FROM users WHERE email = :email";
+    public static final String DELETE_VERIFICATION_CODE_BY_USER_ID = "DELETE FROM tfa_verifications WHERE user_id = :userId";
+    public static final String INSERT_VERIFICATION_CODE_QUERY = "INSERT INTO tfa_verifications (user_id, code, expiration_date) VALUES (:userId, :code, :expirationDate)";
+    public static final String UPDATE_USER_DETAILS_QUERY = "UPDATE users SET first_name = :firstName, last_name = :lastName, email = :email, city = :city, phone = :phone, address = :address, bio = :bio WHERE user_id = :user_id";
+    public static final String SELECT_USER_BY_USER_CODE_QUERY = "SELECT * FROM users WHERE user_id = (SELECT user_id FROM tfa_verifications WHERE code = :code)";
+    public static final String DELETE_CODE = "DELETE FROM tfa_verifications WHERE code = :code";
+    public static final String SELECT_CODE_EXPIRATION_QUERY = "SELECT expiration_date < NOW() AS is_expired FROM tfa_verifications WHERE code = :code";
+    public static final String DELETE_PASSWORD_VERIFICATION_BY_USER_ID_QUERY = "DELETE FROM reset_pass_verifications WHERE user_id = :userId";
+    public static final String INSERT_PASSWORD_VERIFICATION_QUERY = "INSERT INTO reset_pass_verifications (user_id, url, expiration_date) VALUES (:userId, :url, :expirationDate)";
+    public static final String SELECT_USER_BY_PASSWORD_URL_QUERY = "SELECT * FROM users WHERE user_id = (SELECT user_id FROM reset_pass_verifications WHERE url = :url)";
+    public static final String SELECT_EXPIRATION_BY_URL = "SELECT expiration_date < NOW() AS is_expired FROM reset_pass_verifications WHERE url = :url";
+    public static final String UPDATE_USER_PASSWORD_BY_USER_ID_QUERY = "UPDATE users SET password = :password WHERE user_id = :userId";
+    public static final String UPDATE_USER_PASSWORD_BY_ID_QUERY = "UPDATE users SET password = :password WHERE user_id = :userId";
+    public static final String SELECT_USER_BY_ACCOUNT_URL_QUERY = "SELECT * FROM users WHERE user_id = (SELECT user_id FROM account_verifications WHERE url = :url)";
+    public static final String UPDATE_USER_ENABLED_QUERY = "UPDATE users SET enabled = :enabled WHERE userId = :userId";
+    public static final String TOGGLE_USER_MFA_QUERY = "UPDATE users SET using_mfa = :isUsingMfa WHERE email = :email";
+    public static final String UPDATE_USER_SETTINGS_QUERY = "UPDATE users SET enabled = :enabled, non_locked = :notLocked WHERE user_id = :userId";
+    public static final String TOGGLE_USER_NOTIFICATIONS_QUERY = "UPDATE users SET notif_enabled = :notifEnabled WHERE email = :email";
+    public static final String UPDATE_USER_PROFILE_IMAGE_QUERY = "UPDATE users SET image_url = :imageUrl WHERE user_id = :userId";
 }
