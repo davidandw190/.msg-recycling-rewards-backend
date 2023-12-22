ac39ea60be2f57e9e8ae2882cc43560c81650b29
Andrei-David Nan <nandavid190@gmail.com>
2023-12-22 19:19:58 +0200

    v0.4.7 - set up centers list endpoint in RecyclingCentersResource;

---
+++ src/main/java/io/rewardsapp/resource/RecyclingCenterResource.java
@@ -0,0 +1,45 @@
+package io.rewardsapp.resource;
+
+import io.rewardsapp.domain.HttpResponse;
+import io.rewardsapp.domain.RecyclingCenter;
+import io.rewardsapp.dto.UserDTO;
+import io.rewardsapp.service.RecyclingCenterService;
+import io.rewardsapp.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.Optional;
+
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
+
+@RequestMapping(path = "/centers")
+@RequiredArgsConstructor
+public class RecyclingCenterResource {
+    private final UserService userService;
+    private final RecyclingCenterService centerService;
+
+    @GetMapping("/list-all")
+    public ResponseEntity<HttpResponse> listAllRecyclingCenters(
+            @AuthenticationPrincipal UserDTO authenticatedUser,
+            @RequestParam Optional<Integer> page,
+            @RequestParam Optional<Integer> size) {
+
+        return ResponseEntity.ok(
+                HttpResponse.builder()
+                        .timeStamp(LocalDateTime.now().toString())
+                        .data(Map.of(
+                                "user", userService.getUserById(authenticatedUser.id()),
+                                "page", centerService.getRecyclingCenters(page.orElse(0), size.orElse(10))))
+                        .message("Recycling centers retrieved successfully!")
+                        .status(OK)
+                        .statusCode(OK.value())
+                        .build()
+        );
+    }
+}
