d6be0ff4f009477431c175d3e615becd94c71f1c
Andrei-David Nan <nandavid190@gmail.com>
2023-12-25 14:37:40 +0200

    v0.4.16 - configure specs.RecyclingCenterSpecification for searchCenters functionality;

---
+++ src/main/java/io/rewardsapp/specs/RecyclingCenterSpecification.java
@@ -0,0 +1,47 @@
+package io.rewardsapp.specs;
+
+import io.rewardsapp.domain.RecyclableMaterial;
+import io.rewardsapp.domain.RecyclingCenter;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
+import jakarta.persistence.criteria.Predicate;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class RecyclingCenterSpecification {
+
+    public static Specification<RecyclingCenter> searchCenters(
+            String name,
+            String county,
+            String city,
+            String materials
+    ) {
+        return (root, query, criteriaBuilder) -> {
+            List<Predicate> predicates = new ArrayList<>();
+
+            if (StringUtils.hasText(name)) {
+                predicates.add(criteriaBuilder.like(criteriaBuilder.lower(root.get("name")), "%" + name.toLowerCase() + "%"));
+            }
+
+            if (StringUtils.hasText(county)) {
+                predicates.add(criteriaBuilder.equal(criteriaBuilder.lower(root.get("county")), county.toLowerCase()));
+            }
+
+            if (StringUtils.hasText(city)) {
+                predicates.add(criteriaBuilder.equal(criteriaBuilder.lower(root.get("city")), city.toLowerCase()));
+            }
+
+            if (StringUtils.hasText(materials)) {
+                Join<RecyclingCenter, RecyclableMaterial> materialsJoin = root.join("acceptedMaterials", JoinType.LEFT);
+                predicates.add(criteriaBuilder.like(criteriaBuilder.lower(materialsJoin.get("name")), "%" + materials.toLowerCase() + "%"));
+            }
+
+            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+        };
+    }
+}
