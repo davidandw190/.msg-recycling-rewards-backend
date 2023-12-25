623922f5189d867dda3bbdc716b54069ff1f9d7c
Andrei-David Nan <nandavid190@gmail.com>
2023-12-25 14:49:30 +0200

    v0.4.17 - refactor search for center endpoint for search centers with filters;

--- src/main/java/io/rewardsapp/resource/RecyclingCenterResource.java
+++ src/main/java/io/rewardsapp/resource/RecyclingCenterResource.java
@@ -5,7 +5,10 @@ import io.rewardsapp.domain.RecyclingCenter;
 import io.rewardsapp.dto.UserDTO;
 import io.rewardsapp.service.RecyclingCenterService;
 import io.rewardsapp.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.BadRequestException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +18,7 @@ import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.Optional;
 
+import static io.rewardsapp.utils.ExceptionUtils.handleException;
 import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
 
@@ -24,6 +28,8 @@ import static org.springframework.http.HttpStatus.OK;
 public class RecyclingCenterResource {
     private final UserService userService;
     private final RecyclingCenterService centerService;
+    private final HttpServletRequest request;
+    private final HttpServletResponse response;
 
     @GetMapping("/list-all")
     public ResponseEntity<HttpResponse> listAllRecyclingCenters(
@@ -36,7 +42,7 @@ public class RecyclingCenterResource {
                         .timeStamp(LocalDateTime.now().toString())
                         .data(Map.of(
                                 "user", userService.getUserById(authenticatedUser.id()),
-                                "page", centerService.getRecyclingCenters(page.orElse(0), size.orElse(10))))
+                                "page", centerService.getCenters(page.orElse(0), size.orElse(10))))
                         .message("Recycling centers retrieved successfully!")
                         .status(OK)
                         .statusCode(OK.value())
@@ -44,20 +50,57 @@ public class RecyclingCenterResource {
         );
     }
 
+//    @GetMapping("/search")
+//    public ResponseEntity<HttpResponse> searchCustomer(@AuthenticationPrincipal UserDTO user, Optional<String> name, @RequestParam Optional<Integer> page, @RequestParam Optional<Integer> size) {
+//        return ResponseEntity.ok(
+//                HttpResponse.builder()
+//                        .timeStamp(LocalDateTime.now().toString())
+//                        .data(Map.of(
+//                                "user", userService.getUserByEmail(user.email()),
+//                                "page", centerService.searchCenters(name.orElse(""), page.orElse(0), size.orElse(10))))
+//                        .message("Customers retrieved successfully!")
+//                        .status(OK)
+//                        .statusCode(OK.value())
+//                        .build()
+//        );
+//    }
+
     @GetMapping("/search")
-    public ResponseEntity<HttpResponse> searchCustomer(@AuthenticationPrincipal UserDTO user, Optional<String> name, @RequestParam Optional<Integer> page, @RequestParam Optional<Integer> size) {
+    public ResponseEntity<HttpResponse> searchCenters(
+            @AuthenticationPrincipal UserDTO user,
+            @RequestParam(defaultValue = "") String name,
+            @RequestParam(defaultValue = "") String county,
+            @RequestParam(defaultValue = "") String city,
+            @RequestParam(defaultValue = "") String materials,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "createdAt") String sortBy,
+            @RequestParam(defaultValue = "asc") String sortOrder
+    ) {
+
+        Map<String, Object> searchData = null;
+        try {
+            validatePageAndSize(page, size);
+
+            searchData = Map.of(
+                    "user", userService.getUserByEmail(user.email()),
+                    "page", centerService.searchCenters(name, county, city, materials, page, size, sortBy, sortOrder)
+            );
+        } catch (Exception exception) {
+            handleException(request, response, exception);
+        }
+
         return ResponseEntity.ok(
                 HttpResponse.builder()
                         .timeStamp(LocalDateTime.now().toString())
-                        .data(Map.of(
-                                "user", userService.getUserByEmail(user.email()),
-                                "page", centerService.searchCenters(name.orElse(""), page.orElse(0), size.orElse(10))))
-                        .message("Customers retrieved successfully!")
+                        .data(searchData)
+                        .message("Centers retrieved successfully!")
                         .status(OK)
                         .statusCode(OK.value())
                         .build()
         );
     }
+
 
     @PostMapping("/create")
     public ResponseEntity<HttpResponse> createCustomer(@AuthenticationPrincipal UserDTO authenticatedUser, @RequestBody RecyclingCenter newCenter) {
@@ -67,11 +110,17 @@ public class RecyclingCenterResource {
                                 .timeStamp(LocalDateTime.now().toString())
                                 .data(Map.of(
                                         "user", userService.getUserById(authenticatedUser.id()),
-                                        "center", centerService.createRecyclingCenter(newCenter)))
+                                        "center", centerService.createCenter(newCenter)))
                                 .message("Recycling center created successfully!")
                                 .status(CREATED)
                                 .statusCode(CREATED.value())
                                 .build()
                 );
+    }
+
+    private void validatePageAndSize(int page, int size) throws BadRequestException {
+        if (page < 0 || size <= 0 || size > 100) {
+            throw new BadRequestException("Invalid page or size parameters");
+        }
     }
 }
