76b1c81386ee5407c8d092f41bc60cedae615a4c
Andrei-David Nan <nandavid190@gmail.com>
2023-12-25 14:50:57 +0200

    v0.4.18 - set up search centers with configured specification functionality;

--- src/main/java/io/rewardsapp/repository/implementation/RecyclingCenterServiceImpl.java
+++ src/main/java/io/rewardsapp/repository/implementation/RecyclingCenterServiceImpl.java
@@ -3,9 +3,13 @@ package io.rewardsapp.repository.implementation;
 import io.rewardsapp.domain.RecyclingCenter;
 import io.rewardsapp.repository.RecyclingCenterRepository;
 import io.rewardsapp.service.RecyclingCenterService;
+import io.rewardsapp.specs.RecyclingCenterSpecification;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import static org.springframework.data.domain.PageRequest.of;
@@ -17,22 +21,41 @@ public class RecyclingCenterServiceImpl implements RecyclingCenterService {
     private final RecyclingCenterRepository centerRepository;
 
     @Override
-    public Page<RecyclingCenter> getRecyclingCenters(Integer page, Integer size) {
+    public Page<RecyclingCenter> getCenters(Integer page, Integer size) {
         return centerRepository.findAll(PageRequest.of(page, size));
     }
 
     @Override
-    public Iterable<RecyclingCenter> getRecyclingCenters() {
+    public Iterable<RecyclingCenter> getCenters() {
         return centerRepository.findAll();
     }
 
     @Override
-    public RecyclingCenter createRecyclingCenter(RecyclingCenter newCenter) {
+    public RecyclingCenter createCenter(RecyclingCenter newCenter) {
         return centerRepository.save(newCenter);
     }
 
     @Override
     public Page<RecyclingCenter> searchCenters(String name, int page, int size) {
         return centerRepository.findByNameContainingIgnoreCase(name, of(page, size));
+    }
+
+    @Override
+    public Page<RecyclingCenter> searchCenters(
+            String name,
+            String county,
+            String city,
+            String materials,
+            int page,
+            int size,
+            String sortBy,
+            String sortOrder
+    ) {
+        Sort sort = Sort.by(Sort.Direction.fromString(sortOrder), sortBy);
+        Pageable pageable = PageRequest.of(page, size, sort);
+
+        Specification<RecyclingCenter> specification = RecyclingCenterSpecification.searchCenters(name, county, city, materials);
+
+        return centerRepository.findAll(specification, pageable);
     }
 }
--- src/main/java/io/rewardsapp/service/RecyclingCenterService.java
+++ src/main/java/io/rewardsapp/service/RecyclingCenterService.java
@@ -1,16 +1,26 @@
 package io.rewardsapp.service;
 
 import io.rewardsapp.domain.RecyclingCenter;
-import io.rewardsapp.dto.UserDTO;
 import org.springframework.data.domain.Page;
 
 
 public interface RecyclingCenterService {
-    Page<RecyclingCenter> getRecyclingCenters(Integer page, Integer size);
+    Page<RecyclingCenter> getCenters(Integer page, Integer size);
 
-    Iterable<RecyclingCenter> getRecyclingCenters();
+    Iterable<RecyclingCenter> getCenters();
 
-    RecyclingCenter createRecyclingCenter(RecyclingCenter newCenter);
+    RecyclingCenter createCenter(RecyclingCenter newCenter);
 
     Page<RecyclingCenter> searchCenters(String name, int page, int size);
+
+    Page<RecyclingCenter> searchCenters(
+            String name,
+            String county,
+            String city,
+            String materials,
+            int page,
+            int size,
+            String sortBy,
+            String sortOrder
+    );
 }
