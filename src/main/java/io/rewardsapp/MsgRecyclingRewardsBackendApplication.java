e79dc8d59ca763f9276ccb3dcbbead7f047519e4
Andrei-David Nan <nandavid190@gmail.com>
2024-01-11 18:42:08 +0200

    v0.8.6 - enhance verification emails by adding proper dynamic html templates to them, improve async operations;

--- src/main/java/io/rewardsapp/MsgRecyclingRewardsBackendApplication.java
+++ src/main/java/io/rewardsapp/MsgRecyclingRewardsBackendApplication.java
@@ -3,6 +3,7 @@ package io.rewardsapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.cors.CorsConfiguration;
@@ -14,6 +15,7 @@ import java.util.List;
 
 import static io.rewardsapp.constants.SecurityConstants.PASS_ENCODER_STRENGTH;
 
+@EnableAsync
 @SpringBootApplication
 @EnableTransactionManagement
 public class MsgRecyclingRewardsBackendApplication {
--- src/main/java/io/rewardsapp/service/implementation/EmailServiceImpl.java
+++ src/main/java/io/rewardsapp/service/implementation/EmailServiceImpl.java
@@ -3,45 +3,98 @@ package io.rewardsapp.service.implementation;
 import io.rewardsapp.enums.VerificationType;
 import io.rewardsapp.exception.ApiException;
 import io.rewardsapp.service.EmailService;
+import jakarta.mail.BodyPart;
+import jakarta.mail.internet.MimeBodyPart;
+import jakarta.mail.internet.MimeMessage;
+import jakarta.mail.internet.MimeMultipart;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
-    private final JavaMailSender mailSender;
-
     @Value("${spring.mail.username}")
-    private static String FROM;
+    private String FROM;
 
+    public static final String UTF_8_ENCODING = "UTF-8";
+    public static final String EMAIL_TEMPLATE = "verification-email-template";
+    public static final String TEXT_HTML_ENCODING = "text/html";
+
+    private final JavaMailSender mailSender;
+    private final TemplateEngine templateEngine;
+
+    @Async
     @Override
     public void sendVerificationEmail(String firstName, String email, String verificationUrl, VerificationType verificationType) {
-        try{
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setFrom(FROM);
-            message.setTo(email);
-            message.setText(getEmailMessage(firstName, verificationUrl, verificationType));
-            message.setSubject(String.format(".msgRecyclingRewards - %s Verification Email", StringUtils.capitalize(verificationType.getType())));
+        try {
+            MimeMessage message = getMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, UTF_8_ENCODING);
+
+            helper.setPriority(1);
+            helper.setSubject(".msgRecyclingRewards - " + StringUtils.capitalize(verificationType.getType()) + " Verification Email");
+            helper.setFrom(FROM);
+            helper.setTo(email);
+
+            String emailContent = getEmailMessage(firstName, verificationUrl, verificationType);
+
+            Context context = new Context();
+            context.setVariable("name", firstName);
+            context.setVariable("url", verificationUrl);
+            context.setVariable("emailContent", emailContent);
+            context.setVariable("verificationType", verificationType.name());
+
+            String text = templateEngine.process(EMAIL_TEMPLATE, context);
+
+            MimeMultipart mimeMultipart = new MimeMultipart("related");
+
+            BodyPart messageBodyPart = new MimeBodyPart();
+            messageBodyPart.setContent(text, TEXT_HTML_ENCODING);
+            mimeMultipart.addBodyPart(messageBodyPart);
+
+//            BodyPart imageBodyPart = new MimeBodyPart();
+//            DataSource dataSource = new FileDataSource(System.getProperty("user.home") + "/Downloads/images/dog.jpg");
+//            imageBodyPart.setDataHandler(new DataHandler(dataSource));
+//            imageBodyPart.setHeader("Content-ID", "<image>");
+//            mimeMultipart.addBodyPart(imageBodyPart);
+
+            message.setContent(mimeMultipart);
+
             mailSender.send(message);
-            log.info("Email sent to {}", firstName);
+
+            log.info("HTML Email with embedded files sent to {}", firstName);
 
         } catch (Exception exception) {
             log.error(exception.getMessage());
+            throw new RuntimeException(exception.getMessage());
         }
-
     }
 
     private String getEmailMessage(String firstName, String verificationUrl, VerificationType verificationType) {
         switch (verificationType) {
-            case PASSWORD -> { return "Hello " + firstName + "\n\nReset password request. Please click the link below to reset your password. \n\n" + verificationUrl + "\n\nThe RecyclingRewards Support Team"; }
-            case ACCOUNT -> { return "Hello " + firstName + "\n\nYour new account has been created. Please click the link below to verify and enable your account. \n\n" + verificationUrl + "\n\nThe RecyclingRewards Support Team"; }
+            case PASSWORD -> {
+                return "<br/>We have received a request to reset your account password. Please click the link below to securely reset your password:<br/>";
+            }
+            case ACCOUNT -> {
+                return "<br/>Your new account has been successfully created. To enable your account and start enjoying our services, please click the link below:<br/>";
+            }
             default -> throw new ApiException("Unable to send email. Email type unknown");
         }
+    }
+
+    private MimeMessage getMimeMessage() {
+        return mailSender.createMimeMessage();
+    }
+
+    private String getContentId(String filename) {
+        return "<" + filename + ">";
     }
 }
